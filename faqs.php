23942c4791fdbb4ce5e595f2de8d546310194ad2
Abdulrahman Hemed Othman <abdulhemedi99@gmail.com>
2018-02-16 09:49:02 +0300

    Add files via upload

---
+++ faqs.php
@@ -0,0 +1,25 @@
+
+<div class="ui accordion">
+  <div class="title" onclick="$('#c1').slideToggle();">
+    <i class="dropdown icon"></i>
+    What is a Receipt??
+  </div>
+  <div class="content" id="c1">
+    <p class="transition" >A receipt is a written acknowledgement that a person has received money for payment of good or services .</p>
+  </div>
+  <div class="title" onclick="$('#c2').slideToggle();">
+    <i class="dropdown icon"></i>
+    Can i retrieve or re-print a lost receipt?
+  </div>
+  <div class="content" id="c2">
+    <p class="transition">Yes|True, its possible, just provide the #Serial No. above and the system will validate and re-print your receipt.</p>
+  </div>
+ 
+    <div class="title" onclick="$('#c4').slideToggle();">
+    <i class="dropdown icon"></i>
+    Who is the Developer of this system?
+  </div>
+  <div class="content" id="c4">
+    <p class="transition">Good question, The developers are ZanAT Technologies Contact: <a>+255772108420 / +255773321546 </a> or Email me at: <a>zanat-technologies@gmail.com</a> for more inquiries. </p>
+  </div>
+</div>
